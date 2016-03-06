6145c1fc3edc5a0422906ff330a9314123a2e15f
ZiqiXiong <zx012013@mymail.pomona.edu>
2016-03-06 14:33:49 -0800

    functions built on extractBits

--- csim.c
+++ csim.c
@@ -8,6 +8,24 @@ long extractBits(long number, int bitPos, int bitNum)
     return number & mask;
 }
 
+long extractTagBits(long address, int s, int b)
+{
+    return extractBits(address, b+s, 64-b-s);
+}
+
+long extractRowIndex(long address, int s, int b)
+{
+    return extractBits(adress, b, s);
+}
+
+long LeaderOfCacheSlot(long tagBits, int rowIndex, int b)
+{
+    rowIndex <<= b;
+    return tagBits & rowIndex;
+}
+
+
+
 
 int main()
 {
